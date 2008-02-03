2005099bf1b0a2bd6f18c396eb36cf85cf38fb6f
antonio.fabio <antonio.fabio@7d9ea6a8-7623-0410-a856-67f2265a651f>
2008-02-03 12:09:54 +0000

    tagged version 0.6-0

---
+++ tags/0.6-0/tsDyn/R/misc.R
@@ -0,0 +1,53 @@
+extend <- function(...)
+	UseMethod("extend")
+
+extend.list <- function(this, subclass, ..., listV) {
+	class(this) <- c(subclass, class(this))
+	if(missing(listV))
+		return(structure(c(this, list(...)), class=class(this)))
+	else
+		return(structure(c(this, listV), class=class(this)))
+}
+
+MyEnv <- function(...) {
+	this <- new.env()
+	vars <- list(...)
+	nms <- names(vars)
+	if(length(nms)) for(i in 1:length(nms))
+		assign(nms[i], vars[[i]], env=this)
+	structure(this, class=c("MyEnv",class(this)))
+}
+
+extend.MyEnv <- function(this, subclass, ...) {
+	class(this) <- c(subclass, class(this))
+	newvars <- list(...)
+	if(length(names(newvars))) for(nm in names(newvars))
+		assign(nm, newvars[[nm]], env=this)
+	return(this)
+}
+
+availableModels <- function()
+	fitters
+
+latex <- function(obj, ...)
+	UseMethod("latex")
+
+formatSignedNum <- function(x, ...) {
+  signChar <- c("-","+")[(x>=0)+1]
+  nm <- names(x)
+  res <- paste(signChar,format(abs(x), ...) )
+  names(res) <- nm
+  return(res)
+}
+
+build <- function(...)
+	UseMethod("build")
+
+add <- function(...)
+	UseMethod("add")
+
+sigmoid <- function(x) 1/(1 + exp(-x))
+
+dsigmoid <- function(x) sigmoid(x) * (1 - sigmoid(x))
+
+d2sigmoid <- function(x) dsigmoid(x) * (1 - 2 * sigmoid(x))
